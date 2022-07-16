6c71d1cd94fe5795576b7f0e7fde14cc258d31fc
Ashok Pun <ashokpun007@gmail.com>
2022-07-16 11:37:09 +0545

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Feedback App Server
+To serve as API endpoints for the Feedback App built on React
+https://github.com/punashok/feedback-app
---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+const port = process.env.port || 3000
+
+app.get('/', (req, res) => {
+    res.send("Hello Feedback App")
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port} `)
+})
+
+
